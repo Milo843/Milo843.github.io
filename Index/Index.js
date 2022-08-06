40cdda690fcd36beddc5f32e87c77feedbef3a39
Milo843 <mstudzizba@gmail.com>
2022-08-06 16:25:06 +0200

    Making index mobile friendly
    Now hovering over 'photography, graphic design, web development' don't cause photos and videos to appear if screen(media querry) is less than 753
    Index.js now adds class '.show' instead of changing css opacity and visibility attributes

--- Index/Index.js
+++ Index/Index.js
@@ -1,42 +1,42 @@
 
 
-  $(function() {
-    $('.Name').hover(function() {
-      $('.IndexAboutMeContainer').css('visibility', 'visible');
-      $('.IndexAboutMeContainer').css('opacity', '1');
-    }, 
-    function() {
-      // on mouseout, hide pgoto grid
-      $('.IndexAboutMeContainer').css('visibility', 'hidden');
-      $('.IndexAboutMeContainer').css('opacity', '0');
-    });
-  });
 
- 
-  $(function () {
-    $('.C1').hover(function () {
-      $('.IndexPhotoGrid').css('visibility', 'visible');
-      $('.IndexPhotoGrid').css('opacity', '1');
-    },
-      function () {
-        // on mouseout, hide pgoto grid
-        $('.IndexPhotoGrid').css('visibility', 'hidden');
-        $('.IndexPhotoGrid').css('opacity', '0');
-      });
-  });
-  
-  $(function() {
-    $('.C3').hover(function() {
-      $('.IndexVideoFlex').css('visibility', 'visible');
-      $('.IndexVideoFlex').css('opacity', '1');
-      $('.Vid1').get(0).play();
-      $('.Vid1').get(1).play();
+  if (window.matchMedia( "(max-width: 753px)" ).matches) {
+    ;
+  }
+  else {
+    $('.C1').hover(
+      function() {
+        $('.IndexPhotoGrid').addClass("Show");
       },
-     function() {
-      // on mouseout, hide videos flex and pause videos
-      $('.IndexVideoFlex').css('visibility', 'hidden');
-      $('.IndexVideoFlex').css('opacity', '0');
-      $('.Vid1').get(0).pause();
-      $('.Vid1').get(1).pause();
-    });
-  });
+      function() {
+        $('.IndexPhotoGrid').removeClass("Show");
+      }
+    )
+    $('.C3').hover(
+      function() {
+        $('.IndexVideoFlex').addClass("Show");
+        $('.Vid1').get(0).play();
+        $('.Vid1').get(1).play();
+      },
+      function () {
+        $('.IndexVideoFlex').removeClass("Show");
+        $('.Vid1').get(0).pause();
+        $('.Vid1').get(1).pause();
+      }
+    )
+    $('.Name').hover(
+      function() {
+        $('.IndexAboutMeContainer').addClass("Show");
+      },
+      function() {
+        $('.IndexAboutMeContainer').removeClass("Show")
+      }
+    )
+  }
+ 
+
+  
+
+
+  
