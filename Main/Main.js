2bbf5de4d968011b904f0cff9eed38cf7ca9cca3
Milo843 <mstudzizba@gmail.com>
2023-01-20 20:33:24 +0000

    Navbar updated
    Made main.js import navbar form nav/nav.html
    changed 3 <a> navbar to 5 button navbar
    added :root to css in navbar

--- Main/Main.js
+++ Main/Main.js
@@ -1,3 +1,8 @@
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'main/main.css') );
+
+$(function(){
+    $("#main-nav").load("nav/nav.html"); 
+  });
 $(function(){
     $("#Footer").load("Main/Footer.html"); 
   });
