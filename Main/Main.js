d38199395f8921d92370f85ce4e7ac0b010af9cb
Milo843 <mstudzizba@gmail.com>
2023-01-21 12:06:16 +0000

    made navbar load faster

--- Main/Main.js
+++ Main/Main.js
@@ -1,8 +1,6 @@
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'main/main.css') );
 
-$(function(){
-    $("#main-nav").load("nav/nav.html"); 
-  });
+
+
 $(function(){
     $("#Footer").load("Main/Footer.html"); 
   });
