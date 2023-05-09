89371aa6799d097e7fee09789129dcdf2bd90449
Milo843 <mstudzizba@gmail.com>
2023-05-09 19:42:10 +0100

    Huge rework begining (Index rework)
    Index:
    -Added function of video changing in the background when one of the fields of work are hovered over (cgi gives cgi video photography gives photography video)
    -removed the carousel with collections
    -changed a few names and folders

---
+++ Index/slides.js
@@ -0,0 +1,60 @@
+var divPhotography = document.querySelector('#E2');
+var divCgi = document.querySelector('#E4');
+var slidePhotography = document.querySelector('#photography-vid');
+var slideCgi = document.querySelector('#cgi-vid');
+var slidesVisible = document.querySelector('.slide-visible');
+var slides = document.querySelectorAll('.slide');
+
+
+divPhotography.addEventListener('mouseover', function () {
+    slides.forEach(function (slide) {
+        slide.classList.remove('slide-visible');
+    });
+    slides.forEach(function (slide) {
+        slide.classList.add('slide-hidden');
+    });
+    slidePhotography.classList.remove('slide-hidden');
+    slidePhotography.classList.add('slide-visible');
+    playVideos();
+});
+
+
+divCgi.addEventListener('mouseover', function () {
+    slides.forEach(function (slide) {
+        slide.classList.remove('slide-visible');
+    });
+    slides.forEach(function (slide) {
+        slide.classList.add('slide-hidden');
+    });
+    slideCgi.classList.remove('slide-hidden');
+    slideCgi.classList.add('slide-visible');
+    playVideos();
+})
+
+
+/*
+divPhotography.addEventListener('mouseover', function () {
+    slideCgi.classList.add('index-hidden');
+    slidesVisible.classList.remove('index-visible');
+    slidePhotography.classList.add('index-visible');
+
+});
+*/
+
+function playVideos() {
+    const hiddenVideos = document.querySelectorAll('.slide-hidden');
+    const visibleVideos = document.querySelectorAll('.slide-visible');
+
+
+    // Pause hidden videos 
+    hiddenVideos.forEach(function (video) {
+        video.pause();
+    });
+    // Play visible videos instantly
+    visibleVideos.forEach(function (video) {
+        video.play();
+    });
+}
+
+
+
--- Portfolio/main/main.js
+++
@@ -1,8 +0,0 @@
-
-
-$(function(){
-    $("#main-nav").load("nav/nav.html"); 
-  });
-  $(function(){
-    $("#footer").load("main/footer.html"); 
-  });
